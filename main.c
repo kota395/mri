8c4ed8d46fa3005178111c2675bbe0dc05e8d198
kota395 <kt395@outlook.jp>
2018-10-20 17:23:01 +0900

    add Makefile, .gitignore

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
+#include <stdlib.h>
 
+int main(){
 
-int main()
+}
+
+int file_io(char *filename,char *base_sheet){
+  FILE *fp;
+  char ch;
+
+  if ((fp = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "%sのオープンに失敗しました.\n", filename);
+    exit(EXIT_FAILURE);
+  }
+  
+  while (( ch = fgetc(fp)) != EOF ) {
+    putchar(ch);
+  }
+  
+  fclose(fp);
+}
+
+char *symbol_to_meaning(char ch){
+  char *meaning;
+  
+  return meaning;
+}
+
 
